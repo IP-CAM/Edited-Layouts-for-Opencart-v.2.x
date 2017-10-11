c9d39b2deee1b0213040bb8eda43d43836882bf7
agliullin <albertagliullin@gmail.com>
2017-10-11 22:53:57 +0300

    Add original opencart files

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart.web/admin/');
+define('HTTP_CATALOG', 'http://opencart.web/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart.web/admin/');
+define('HTTPS_CATALOG', 'http://opencart.web/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Server/OpenServer/domains/opencart.web/admin/');
+define('DIR_SYSTEM', 'D:/Server/OpenServer/domains/opencart.web/system/');
+define('DIR_IMAGE', 'D:/Server/OpenServer/domains/opencart.web/image/');
+define('DIR_LANGUAGE', 'D:/Server/OpenServer/domains/opencart.web/admin/language/');
+define('DIR_TEMPLATE', 'D:/Server/OpenServer/domains/opencart.web/admin/view/template/');
+define('DIR_CONFIG', 'D:/Server/OpenServer/domains/opencart.web/system/config/');
+define('DIR_CACHE', 'D:/Server/OpenServer/domains/opencart.web/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Server/OpenServer/domains/opencart.web/system/storage/download/');
+define('DIR_LOGS', 'D:/Server/OpenServer/domains/opencart.web/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Server/OpenServer/domains/opencart.web/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Server/OpenServer/domains/opencart.web/system/storage/upload/');
+define('DIR_CATALOG', 'D:/Server/OpenServer/domains/opencart.web/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'opencart.web');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opencart.web');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
